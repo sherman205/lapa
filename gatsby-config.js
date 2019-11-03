10e89a83a235ff4cd3bba6d42c0e7ef55fcc570c
Sophia <sherman205@gmail.com>
2019-11-02 17:37:06 -0700

    Create first markdown graphql content for about us page

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,29 @@ module.exports = {
     title: `Lapa`,
     description: `Food blog built with React and Gatsby`
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,29 +1,66 @@
 import React from 'react';
+import Img from "gatsby-image";
+import { graphql } from "gatsby";
 import NavSmall from '../components/navSmall';
 import FooterSmall from '../components/footerSmall';
 import '../styles/about.scss';
 
-const About = () => {
+const About = ({data}) => {
+    const title = (data.allMarkdownRemark.edges)[0].node.frontmatter.title;
+    const intro = (data.allMarkdownRemark.edges)[0].node.frontmatter.intro;
+    const whoWeAre = (data.allMarkdownRemark.edges)[0].node.frontmatter.whoWeAre;
+    const whatIsLapa = (data.allMarkdownRemark.edges)[0].node.frontmatter.whatIsLapa;
+    const aboutSite = (data.allMarkdownRemark.edges)[0].node.frontmatter.aboutSite;
+    const img = (data.allMarkdownRemark.edges)[0].node.frontmatter.featuredImage.childImageSharp.fluid;
+
     return (
         <div className="about">
             <div className="about-left">
                 <NavSmall />
                 <div className="about-content">
-                    <p className="title playfair"></p>
-                    
-                    <h3>What Is Lapa</h3>
-                    
+                    <p className="title playfair">{title}</p>
+                    <div>{intro}</div>
                     <h3>Who We Are</h3>
-                    
+                    <div>{whoWeAre}</div>
+                    <h3>What Is Lapa</h3>
+                    <div>{whatIsLapa}</div>
                     <h3>The Site And Beyond</h3>
-                    
+                    <div>{aboutSite}</div>
                 </div>
                 <FooterSmall />
             </div>
             <div className="about-right">
+                <Img className="img" fluid={img} alt="About" />
             </div>
         </div>
     );
 }
+
+export const query = graphql`
+query {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+            intro
+            whoWeAre
+            whatIsLapa
+            aboutSite
+            featuredImage{
+                childImageSharp {
+                    fluid {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default About;
