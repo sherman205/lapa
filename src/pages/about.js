f393177d6dc331853dddbc6843b76ee6d90e017e
Sophia <sherman205@gmail.com>
2019-11-06 10:49:31 -0800

    Testing window resize for mobile layout in prod builds

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { isMobile } from "react-device-detect";
+import React, { Component } from 'react';
+// import { isMobile } from "react-device-detect";
 import Img from "gatsby-image";
 import { graphql } from "gatsby";
 import NavSmall from '../components/navSmall';
@@ -8,48 +8,114 @@ import NavMobile from '../components/navMobile';
 import Root from '../components/root';
 import '../styles/about.scss';
 
-const About = ({data}) => {
-    const { title, intro } = (data.about.edges)[0].node.frontmatter;
-    const img = (data.about.edges)[0].node.frontmatter.featuredImage.childImageSharp.fluid;
+export default class About extends Component {
+  constructor(props) {
+      super(props);
+      const width = typeof window !== `undefined` ? window.innerWidth : null;
+      this.state = {
+          width: width
+      };
+  };
 
-    if (!isMobile) {
-      return (
-        <>
-          <Root metadata={data.metadata.siteMetadata} />
-          <div className="about">
-              <div className="about-left">
-                  <NavSmall />
-                  <div className="about-content">
-                      <p className="title playfair">{title}</p>
-                      <div dangerouslySetInnerHTML={{ __html: (data.about.edges)[0].node.html }} />
-                  </div>
-                  <FooterSmall />
-              </div>
-              <div className="about-right">
-                  <Img className="img" fluid={img} alt="About" />
-              </div>
-          </div>
-        </>
-      );
-    }
-  else {
-    return (
-      <>
-        <Root metadata={data.metadata.siteMetadata} />
-        <NavMobile />
-        <div className="mobile-about">
-            <div className="about-content">
-              <p className="header playfair">{title}</p>
-              <div>{intro}</div>
-              <Img className="img" fluid={img} alt="About" />
-              <div dangerouslySetInnerHTML={{ __html: (data.about.edges)[0].node.html }} />
+  componentWillMount = () => {
+      window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+  componentWillUnmount = () => {
+      window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+  handleWindowSizeChange = () => {
+      this.setState({ width: window.innerWidth });
+  };
+
+  render() {
+      const { width } = this.state;
+      const { data } = this.props;
+      const { title, intro } = (data.about.edges)[0].node.frontmatter;
+      const img = (data.about.edges)[0].node.frontmatter.featuredImage.childImageSharp.fluid;
+      const isMobile = width <= 650;
+
+      if (!isMobile) {
+        return (
+          <>
+            <Root metadata={data.metadata.siteMetadata} />
+            <div className="about">
+                <div className="about-left">
+                    <NavSmall />
+                    <div className="about-content">
+                        <p className="title playfair">{title}</p>
+                        <div dangerouslySetInnerHTML={{ __html: (data.about.edges)[0].node.html }} />
+                    </div>
+                    <FooterSmall />
+                </div>
+                <div className="about-right">
+                    <Img className="img" fluid={img} alt="About" />
+                </div>
             </div>
-            <FooterSmall />
-        </div>
-      </>
-    );
-  }  
+          </>
+        );
+      }
+      else {
+        return (
+          <>
+            <Root metadata={data.metadata.siteMetadata} />
+            <NavMobile />
+            <div className="mobile-about">
+                <div className="about-content">
+                  <p className="header playfair">{title}</p>
+                  <div>{intro}</div>
+                  <Img className="img" fluid={img} alt="About" />
+                  <div dangerouslySetInnerHTML={{ __html: (data.about.edges)[0].node.html }} />
+                </div>
+                <FooterSmall />
+            </div>
+          </>
+        );
+      }
+  }
 }
+
+// const About = ({data}) => {
+//     const { title, intro } = (data.about.edges)[0].node.frontmatter;
+//     const img = (data.about.edges)[0].node.frontmatter.featuredImage.childImageSharp.fluid;
+
+//     if (!isMobile) {
+//       return (
+//         <>
+//           <Root metadata={data.metadata.siteMetadata} />
+//           <div className="about">
+//               <div className="about-left">
+//                   <NavSmall />
+//                   <div className="about-content">
+//                       <p className="title playfair">{title}</p>
+//                       <div dangerouslySetInnerHTML={{ __html: (data.about.edges)[0].node.html }} />
+//                   </div>
+//                   <FooterSmall />
+//               </div>
+//               <div className="about-right">
+//                   <Img className="img" fluid={img} alt="About" />
+//               </div>
+//           </div>
+//         </>
+//       );
+//     }
+//   else {
+//     return (
+//       <>
+//         <Root metadata={data.metadata.siteMetadata} />
+//         <NavMobile />
+//         <div className="mobile-about">
+//             <div className="about-content">
+//               <p className="header playfair">{title}</p>
+//               <div>{intro}</div>
+//               <Img className="img" fluid={img} alt="About" />
+//               <div dangerouslySetInnerHTML={{ __html: (data.about.edges)[0].node.html }} />
+//             </div>
+//             <FooterSmall />
+//         </div>
+//       </>
+//     );
+//   }  
+// }
 
 export const query = graphql`
 {
@@ -83,4 +149,4 @@ export const query = graphql`
   }
 `;
 
-export default About;
+// export default About;
