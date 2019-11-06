4057737ebb1dfb4ceec26cc24755457f1ced1902
Sophia <sherman205@gmail.com>
2019-11-05 18:41:17 -0800

    Add search box to 404 page to search again when no results found

---
+++ src/components/searchNoResults.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { navigate } from 'gatsby-link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import './styles/searchNoResults.scss';
+
+class SearchNoResults extends Component {
+    state = {
+        searchString: ""
+    }
+
+	handleChange = () => {
+	    this.setState({
+	    	searchString: this.refs.search.value
+	    });
+	}
+
+	clearForm = () => {
+		this.setState({
+			searchString: ""
+		});
+		this.refs.search.focus();
+	}
+
+	onSearch = (e) => {
+		e.preventDefault();
+		if (this.state.searchString !== "") {
+            navigate(`/results/${this.state.searchString}`);
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<form className="search-form" onSubmit={this.onSearch}>
+					<input 
+					className="search-bar"
+					id="search"
+					type="text"
+					value={this.state.searchString}
+					ref="search"
+					onChange={this.handleChange}
+					placeholder="Search for something"
+					/>
+					<FontAwesomeIcon className="search-icon" icon={faSearch} onClick={this.onSearch} />
+				</form>
+			</div>
+
+		);
+	}
+}
+
+export default SearchNoResults;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavSmall from '../components/navSmall';
 import FooterSmall from '../components/footerSmall';
+import SearchNoResults from '../components/searchNoResults';
 import Root from '../components/root';
 
 import '../styles/404.scss';
@@ -13,6 +14,7 @@ const NoResults = ( {data} ) => {
             <div className="recipe-filtered">
                 <div className="results">
                     <h3>No results found -- try entering different keywords:</h3>
+                    <SearchNoResults />
                 </div>
             </div>
             <FooterSmall />
